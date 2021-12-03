957d248e9acff0e52f0ff229093de30f3a4b696e
Klondike89 <brianjmurphy0830@gmail.com>
2021-12-03 14:29:39 -0600

    This completes day1

--- 2021_Aoc_Day1.py
+++ 2021_Aoc_Day1.py
@@ -1,10 +1,26 @@
 with open(r'C:\Users\brian\Documents\GitHub\2021-Advent-of-Code\2021_AoC_Day1_input.txt','r') as file:
     x = 0
-    old_line = file.readline()
-    print(old_line)
-    #import pdb; pdb.set_trace()
+    old_window = []
+    new_window = []
+    old_window.append(int(file.readline()))
+    for line in range(2):
+        start = int(file.readline())
+        old_window.append(start)
+        new_window.append(start)
+    # print(old_window)
+    # print(new_window)
+    # import pdb; pdb.set_trace()
     for line in file:
-        if int(line) > int(old_line):
+        # print(old_window)
+        # print(new_window)
+        new_window.append(int(line))
+        # print(old_window)
+        # print(new_window)
+        if sum(new_window) > sum(old_window):
             x += 1
-        old_line = line
+        del old_window[0]
+        del new_window[0]
+        old_window.append(int(line))
+        # print(old_window)
+        # print(new_window)
 print(x)
