862efff59ecbdd6981eeee6108eb01eb3c655f10
Klondike89 <brianjmurphy0830@gmail.com>
2021-12-08 14:34:03 -0600

    FINALLY WORKS
    bingo.py

--- bingo.py
+++ bingo.py
@@ -26,6 +26,9 @@ def update_boards(board):
 """ Calculate total value of all uncalled values on board then multiply by drawn number """
 def board_total(board):
     larry = [x for x in board if x != 100]
+    for x in range(5):
+        x = x*5
+        print(board[x:x+5])
     print(larry)
     print(num)
     total = sum(larry)*num
@@ -36,11 +39,13 @@ def is_win_case(board):
     i = 0
     while i <= 20:
         if sum([board[i],board[i+1],board[i+2],board[i+3],board[i+4]]) == 500:
+            board_total(board)
             return True
         i += 5
     i = 0
     while i <= 4:
         if sum([board[i],board[i+5],board[i+10],board[i+15],board[i+20]]) == 500:
+            board_total(board)
             return True
         i += 1
 
@@ -59,7 +64,11 @@ while found_all == False:
                 boards.pop(play_index)
             else:
                 found_all = True
+                play_index += 1
+            #bp()
         else:
             play_index += 1
-    print(play_index)
+    print(len(boards))
+    #bp()
+
 print(boards)
