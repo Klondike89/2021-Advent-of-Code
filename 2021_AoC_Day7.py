dec6122ad3dba20bbb5e41eb738e639ddb03083d
Klondike89 <brianjmurphy0830@gmail.com>
2021-12-14 20:21:23 -0600

    Solved Day7 Part 2

--- 2021_AoC_Day7.py
+++ 2021_AoC_Day7.py
@@ -1,25 +1,25 @@
 from pdb import set_trace as bp
-from statistics import median
 
+# Input file 
 f = r'C:\Users\brian\Documents\GitHub\2021-Advent-of-Code\2021_AoC_Day7_input.txt'
 
+""" Get Data from Input File"""
 with open(f,'r') as file:
     data = file.readline().split(',')
     for x in range(len(data)):
         data[x] = int(data[x])
     data.sort()
-
-# print(data)
-# print(data[len(data)-1])
+ 
+""" Test Only Data """
 # data = [16,1,2,0,4,2,7,1,2,14]
 
+""" Part 1 """
 totals = []
 for x in range(data[-1]):
     point_distances = []
     for i in data:
         distance_from = abs(x-i)
         point_distances.append(distance_from)
-        # print(point_distances)
     total = sum(point_distances)
     totals.append(total)
 min_value = min(totals)
@@ -30,6 +30,19 @@ for i in data:
     add_fuel = abs(min_index-i)
     fuel_total = fuel_total + add_fuel
 
-print(fuel_total)
+print("Fuel Needed Part-1: " + str(fuel_total))
 
+""" Part 2 """
+totals = []
+for x in range(data[-1]):
+    fuel_needs = []
+    for i in data:
+        distance_from = abs(x-i)
+        fuel_needed = (distance_from+1)*(distance_from/2)
+        fuel_needs.append(fuel_needed)
+    total = sum(fuel_needs)
+    totals.append(total)
+min_value = min(totals)
+
+print("Fuel Needed Part-2: " + str(min_value))
 # bp()
